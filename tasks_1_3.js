94ea7fe37d03050ad7fad8a6bd3d5f2dfcada05b
Evgeniy-makdak <makdak23@mail.ru>
2025-05-19 10:03:50 +0300

    Добавлены две новые таски

---
+++ tasks_1_3.js
@@ -0,0 +1,20 @@
+console.log("Уровень 1.3 задачника code.me");
+// Дана строка. Если в этой строке более одного символа, выведите в консоль предпоследний символ этой строки.
+const isStringToPreLastSymbol = "предпоследний символ";
+isStringToPreLastSymbol.length > 1
+  ? console.log(
+      `Предпоследний символ строки ${isStringToPreLastSymbol}: ${isStringToPreLastSymbol.at(
+        -2
+      )}`
+    )
+  : console.log("строка состоит из одного символа");
+// Даны два целых числа. Проверьте, что первое число без остатка делится на второе.
+const firstNumber1_3 = 11;
+const secondNumber1_3 = 5;
+firstNumber1_3 % secondNumber1_3 === 0
+  ? console.log(
+      `Число ${firstNumber1_3} делится на ${secondNumber1_3} без остатка`
+    )
+  : console.log(
+      `Число ${firstNumber1_3} не делится на ${secondNumber1_3} без остатка`
+    );
