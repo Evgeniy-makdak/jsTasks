15926b6c9116bb3365c63a527dfb4b3315ce0ccc
Evgeniy-makdak <makdak23@mail.ru>
2025-06-29 19:29:07 +0300

    Задачи из 1_10

---
+++ tasks_1_10.js
@@ -0,0 +1,24 @@
+console.log("Уровень 1.10 задачника code.me");
+console.log(
+  "Заполните массив случайными целыми числами из промежутка от 1 до 100."
+);
+let randomArray = [];
+while (randomArray.length < 10) {
+  randomArray.push(+(Math.random(0, 1) * 10).toFixed(0));
+}
+console.log(randomArray);
+console.log(
+  "Дано некоторое число: 12345. Выведите в консоль все его символы с конца."
+);
+const defaultNumber = "12345";
+console.log(defaultNumber.toString().split("").reverse().toString());
+console.log([...defaultNumber].reverse().toString());
+console.log(
+  "Дано некоторое число: 12345. Выведите в консоль все его символы с конца."
+);
+console.log(
+  "Дан некоторый массив, например, вот такой: [1, 2, 3, 4, 5, 6]. По очереди выведите в консоль подмассивы из двух элементов нашего массива:" 
+// [1, 2]
+// [3, 4]
+// [5, 6]
+)
