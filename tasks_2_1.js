1b743bfa1c8b9046664f813f030bd79b2c53bb17
Evgeniy-makdak <makdak23@mail.ru>
2025-07-08 10:19:27 +0300

    Решение всех задач темы 2.1

--- tasks_2_1.js
+++ tasks_2_1.js
@@ -11,6 +11,7 @@ for (let i = 0; i < stringWithZero.length; i++) {
 console.log(
   `Первый нуль в строке ${stringWithZero} ${positionOfFirstZero + 1} по счёту`
 );
+console.log("**************************************************************************************************")
 console.log(
   "Выведите в консоль все числа в промежутке от 1 до 1000, сумма первой и второй цифры которых равна пяти."
 );
@@ -21,3 +22,15 @@ for (let i = 1; i <= 1000; i++) {
     }
 }
 console.log(summerOfFirstAndSecondasNumbers);
+console.log("**************************************************************************************************")
+console.log("Дан массив. Удалите из него элементы с заданным значением.");
+const defaultArray = [37, 'array', {name: 'Poluect', age: '731'}, "78", 12, ["7", 7, 'word']];
+const newArray = defaultArray.filter((elem) => typeof(elem) === 'object')
+console.log(newArray);
+console.log("**************************************************************************************************")
+console.log("Дан некоторый массив, например, вот такой:");
+console.log("[1, 2, 3, 4, 5, 6]");
+console.log("Найдите сумму первой половины элементов этого массива.");
+const simpleArray = [1, 2, 3, 4, 5, 6];
+const summerOfFirstsElements = (simpleArray.slice(0, simpleArray.length / 2).reduce((acc, el) => acc + el));
+console.log(summerOfFirstsElements);
