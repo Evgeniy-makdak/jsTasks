f3f9a0e6aa60f3536cdb7c662c593e7c5fffc76f
Evgeniy-makdak <makdak23@mail.ru>
2025-07-18 10:53:26 +0300

    Решены задачи 2 и 3 из уровня 2.4

--- tasks_2_4.js
+++ tasks_2_4.js
@@ -11,5 +11,29 @@ for (let i = 0; i < randomString.length; i++) {
 }
     console.log(`Первая цифра в строке ${randomString} находится на позиции ${codeOfFirstNumber}`);
 console.log("**************************************************************************************************")
-// const firstNumber = randomString.indexOf(filter((el) => el.charCodeAt() >= 48 && el.charCodeAt() <= 57))
-// console.log(firstNumber);
+console.log("Дан объект с ключами и значениями. Запишите в первый массив ключи объекта, а во второй - значения.");
+const baseObject = {
+    mark: "Tesla",
+    type: "sedan",
+    year: "2027",
+    color: "gray",
+}
+const keysArray = [];
+const valuesArray = [];
+function destrictObject() {
+    for (const key of Object.keys(baseObject)) {
+        keysArray.push(key)
+    }
+    console.log(keysArray);
+    for (const value of Object.values(baseObject)) {
+        valuesArray.push(value);
+    }
+    console.log(valuesArray);
+}
+destrictObject();
+console.log("**************************************************************************************************")
+console.log("Дано число. Выведите в консоль количество четных цифр в этом числе.")
+const randomNumber = 5237118;
+const evenNumbersCount = randomNumber.toString().split('').filter((numb) => numb % 2 === 0).length;
+console.log(`Количество чётных цифр в числе ${randomNumber} равно ${evenNumbersCount}`);
+console.log("**************************************************************************************************")
