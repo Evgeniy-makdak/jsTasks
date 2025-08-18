84aa02f74c08cac2521cb2f490fd15028436bfe2
Evgeniy-makdak <makdak23@mail.ru>
2025-08-18 10:51:43 +0300

    Решены задачи 3 и 4 уровня 3.1

--- tasks_3_1.js
+++ tasks_3_1.js
@@ -19,3 +19,30 @@ console.log("Удалите из массива все пустые строки
 const defaultArray3_1_2 = [1, '', 2, 3, '', 5];
 const defaultArray3_1_2NoEmpty = defaultArray3_1_2.filter((el) => el !== '');
 console.log(defaultArray3_1_2NoEmpty);
+console.log(
+    "**************************************************************************************************"
+);
+console.log("Дан массив:");
+console.log("[");
+console.log("[2, 1, 4, 3, 5],");
+console.log("[3, 5, 2, 4, 1],");
+console.log("[4, 3, 1, 5, 2],");
+console.log("]");
+console.log("Отсортируйте элементы в каждом подмассиве.");
+const defaultArray3_1_3 = [
+    [2, 1, 4, 3, 5],
+    [3, 5, 2, 4, 1],
+    [4, 3, 1, 5, 2],
+]
+const sortArray = defaultArray3_1_3.map((arr) => arr.sort((a, b) => a - b));
+console.log(sortArray);
+console.log(
+    "**************************************************************************************************"
+);
+console.log("Даны два массива:");
+console.log("let arr1 = [1, 2, 3];");
+console.log("let arr2 = [1, 2, 3, 4, 5];");
+console.log("Удалите из большего массива лишние элементы с конца так, чтобы длины массивов стали одинаковыми.");
+const defaultArray1_3_1_4 = [1, 2, 3];
+const defaultArray2_3_1_4 = [73, 1, 2, 3, 4, 5, 22];
+console.log(defaultArray2_3_1_4.slice(0, defaultArray1_3_1_4.length));
