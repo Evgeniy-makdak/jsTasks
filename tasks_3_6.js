cd4565ae253ffbf62e9ea9551a1f840bebd0ba45
Evgeniy-makdak <makdak23@mail.ru>
2025-09-03 11:05:05 +0300

    Решения задач 3 и 4 уровня 3.6

--- tasks_3_6.js
+++ tasks_3_6.js
@@ -23,3 +23,53 @@ console.log("[123, 456, 789]");
 console.log("Слейте все элементы этого массива в один массив, разбив их посимвольно:");
 console.log("[1, 2, 3, 4, 5, 6, 7, 8, 9]");
 const array3_6_3 = [123, 456, 789];
+const mutateArray3_6_3 = array3_6_3.map((arr) => [...arr.toString()]);
+resultArray3_6_3 = []
+for (let i = 0; i < mutateArray3_6_3.length; i++) {
+    resultArray3_6_3.push(...mutateArray3_6_3[i])
+}
+console.log((resultArray3_6_3.map((el) => +el)))
+console.log(
+    "**************************************************************************************************"
+);
+console.log("Дан следующая структура:");
+console.log("let data = [");
+console.log(" {");
+console.log("  1: 11,");
+console.log("  2: 12,");
+console.log("  3: 13,");
+console.log(" },");
+console.log(" 2: {");
+console.log("  1: 21,");
+console.log("  2: 22,");
+console.log("  3: 23,");
+console.log(" },");
+console.log(" 3: {");
+console.log("  1: 24,");
+console.log("  2: 25,");
+console.log("  3: 26,");
+console.log(" },");
+console.log("]");
+console.log("Найдите сумму элементов этой структуры.");
+const data3_6_4 = [
+	{
+		1: 11,
+		2: 12,
+		3: 13,
+	},
+	{
+		1: 21,
+		2: 22,
+		3: 23,
+	},
+	{
+		1: 24,
+		2: 25,
+		3: 26,
+	},
+];
+console.log(Object.values(data3_6_4).map((arr) => Object.values(arr)).map((el) => el.reduce((acc, e) => {
+	return +acc + +e;
+})).reduce((acc, el) => {
+	return +acc + +el
+}));
