6a565eb1b243e19fe0961ecb5e42bd4f9ba0b38d
Evgeniy-makdak <makdak23@mail.ru>
2025-10-07 09:21:39 +0300

    Решение задачи 3 уровня 4.3

--- tasks_4_3.js
+++ tasks_4_3.js
@@ -20,5 +20,20 @@ function no3Doubles(arr) {
     return arr.filter(element => countMap[element] <= 3);
 }
 
-const array4_3_2 = [3, 7, 3, 'a', 'this', 'a', 'a', 'store', 77, "", 'store', 0.77, 'store', 'store', 3];
+const array4_3_2 = [3, 7, 3, 'a', 'this', 'a', 'a', 'store', 77, "", 'store', 0.77, 'store', 'store', 3, "77", "77"];
 console.log("Результат:", no3Doubles(array4_3_2));
+console.log(
+    "**************************************************************************************************"
+);
+console.log("№3: Сделайте функцию, которая параметром будет принимать массив и удалять из него одинаковые, рядом стоящие элементы.");
+const mutateArray4_3_3 = [];
+function deleteDoubles() {
+    for (let i = 0; i < array4_3_2.length; i++) {
+        if (array4_3_2[i] !== array4_3_2[i + 1]) {
+            mutateArray4_3_3.push(array4_3_2[i]);
+        };
+    };
+    return mutateArray4_3_3;
+};
+
+console.log(deleteDoubles());
