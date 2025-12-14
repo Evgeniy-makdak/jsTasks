d8ca2164eaa5862fc0f4035402a490c3a7d40b0c
Evgeniy-makdak <makdak23@mail.ru>
2025-12-14 20:15:05 +0300

    Решение задачи 4 и 5 уровня 4.4

--- tasks_4_4.js
+++ tasks_4_4.js
@@ -15,18 +15,44 @@ console.log(
     "**************************************************************************************************"
 );
 
-console.log("#б/н: Сделайте функцию, которая будет выводить произвольное сообщение в консоль через 7 секунд.");
+// console.log("#б/н: Сделайте функцию, которая будет выводить произвольное сообщение в консоль через 7 секунд.");
 
-function sleep(ms) {
-    return new Promise(resolve => 
-       setTimeout(resolve, ms)
-    )
+// function sleep(ms) {
+//     return new Promise(resolve => 
+//        setTimeout(resolve, ms)
+//     )
+// }
+
+// async function time() {
+//     console.log('отсчёт начался');
+//     await sleep(7000);
+//     console.log('прошло ровно 7 секунд');
+// };
+
+// time();
+console.log(
+    "**************************************************************************************************"
+);
+console.log("№2: Сделайте функцию, которая параметром будет принимать число и проверять, простое оно или нет.");
+const slowNumber = (number) => {
+    let count = 0;
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            count++;
+        }
+    }
+    count > 1 ? console.log(`Число ${number} не является простым.`)  : console.log(`число ${number} простое.`)
 }
-
-async function time() {
-    console.log('отсчёт начался');
-    await sleep(7000);
-    console.log('прошло ровно 7 секунд');
-};
-
-time();
+slowNumber(7)
+console.log(
+    "**************************************************************************************************"
+);
+console.log("№3: Сделайте функцию, которая параметром будет принимать число и удалять из него четные цифры.");
+const evelNumbers = (number) => {
+    const arrFromNumber = [...number.toString()].filter((unit) => +unit % 2 !== 0).join('');
+    return arrFromNumber;
+}
+console.log(evelNumbers(52388));
+console.log(
+    "**************************************************************************************************"
+);
