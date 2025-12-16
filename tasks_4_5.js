16f06a3ed1265981b2a3b52c0d6073b82e65f438
Evgeniy-makdak <makdak23@mail.ru>
2025-12-16 11:49:14 +0300

    Решение задачи 1 уровня 4.5

--- tasks_4_5.js
+++ tasks_4_5.js
@@ -1,6 +1,21 @@
 console.log("Уровень 4.5 задачника code.me");
-console.log("№1: Дана переменная со строкой. Проверьте, что в эта строка представляет собой число, то есть состоит только из цифр.");
+console.log("№1: Дана переменная со строкой. Проверьте, что эта строка представляет собой число, то есть состоит только из цифр.");
+const string = '31500t1';
 
+function isThatNumbers(string) {
+    const allDigits = [...string].every((char) => {
+        const code = char.charCodeAt(0);
+        return code >= 48 && code <= 57;
+    });
+    
+    if (allDigits) {
+        console.log(`Строка "${string}" - это число!`);
+    } else {
+        console.log(`В строке "${string}" есть не числовые символы.`);
+    }
+}
+
+isThatNumbers(string);
 console.log(
     "**************************************************************************************************"
 );
