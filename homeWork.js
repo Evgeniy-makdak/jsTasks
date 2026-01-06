e033aa2d7b23441bc8604884f09313ffdb8fc6fe
Evgeniy-makdak <makdak23@mail.ru>
2026-01-06 21:37:33 +0300

    Решил задачу 4 из hw

--- homeWork.js
+++ homeWork.js
@@ -1,32 +1,32 @@
 console.log("№1 Разворот строки");
 
 function reverseString(string) {
-    console.log([...string].reverse().join(""));
+  console.log([...string].reverse().join(""));
 }
 
 reverseString("maxcode.dev");
 // 30 секунд;
 console.log(
-    "**************************************************************************************************"
+  "**************************************************************************************************"
 );
-console.log("№3 Панграмма" );
+console.log("№3 Панграмма");
 
 function isPangram(sentence) {
-    const alphabet = "abcdefghijklmnopqrstuvwxyz";
-    const lowerSentence = sentence.toLowerCase();
-    for (let i = 0; i < alphabet.length; i++) {
-        if (!lowerSentence.includes(alphabet[i])) {
-            return false;
-        }
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const lowerSentence = sentence.toLowerCase();
+  for (let i = 0; i < alphabet.length; i++) {
+    if (!lowerSentence.includes(alphabet[i])) {
+      return false;
     }
-    
-    return true;
+  }
+
+  return true;
 }
 
 console.log(isPangram("The quick brown fox jumps over he lazy dog"));
 // Приблизительно 3 минуты;
 console.log(
-    "**************************************************************************************************"
+  "**************************************************************************************************"
 );
 console.log("№2 Top Words");
 
@@ -58,14 +58,17 @@ const words = [
   "agree",
 ];
 function topWords(words, query, limit) {
-    const resWords = [];
-    for (let i = 0; i < words.length; i++) {
-        if (words[i].toLowerCase().includes(query.toLowerCase()) && words[i][0].toLowerCase() === query[0].toLowerCase()) {
-            resWords.push(words[i]);
-        }
+  const resWords = [];
+  for (let i = 0; i < words.length; i++) {
+    if (
+      words[i].toLowerCase().includes(query.toLowerCase()) &&
+      words[i][0].toLowerCase() === query[0].toLowerCase()
+    ) {
+      resWords.push(words[i]);
     }
-    return resWords.splice(0, limit);
-};
+  }
+  return resWords.splice(0, limit);
+}
 
 console.log(topWords(words, "Af", 3).join(","));
 console.log(topWords(words, "aga", 5).join(","));
@@ -73,5 +76,19 @@ console.log(topWords(words, "b", 2).join(","));
 
 // Около 4 минут;
 console.log(
-    "**************************************************************************************************"
+  "**************************************************************************************************"
 );
+console.log("№4 Функция every");
+function every(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    const result = callback(array[i], i, array);
+    if (!result) return false;
+  }
+  return true;
+}
+
+console.log(every(["a", "b", "c", "d"], (x, i) => x.length >= i));
+console.log(
+  "**************************************************************************************************"
+);
+// console.log("№4 Функция every");
